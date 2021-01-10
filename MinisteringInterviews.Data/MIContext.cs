dedfabad9a3fba1caf26313063d7a7359f91668f
leadensky <ben@leadensky.com>
2021-01-10 15:31:46 -0700

    Removed API project to start over. :P

--- MinisteringInterviews.Data/MIContext.cs
+++ MinisteringInterviews.Data/MIContext.cs
@@ -6,6 +6,11 @@ namespace MinisteringInterviews.Data
 {
     public class MIContext : DbContext
     {
+        public MIContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
         public DbSet<Person> People { get; set; }
         public DbSet<Companionship> Companionships { get; set; }
         public DbSet<Appointment> Appointments { get; set; }
